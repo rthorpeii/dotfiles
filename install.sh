0f9324b6561c1e8009efaca5bb742301a9868c32
Robert Thorpe II <rthorpeii@github.com>
2022-01-25 14:01:09 -0500

    Adding gitconfig

--- install.sh
+++ install.sh
@@ -2,29 +2,26 @@
 
 setup-omz() {
     echo "==========================================================="
-    echo "* Installing Oh-My-Zsh and extensions..."
+    echo "* Installing Oh-My-Zsh"
     echo "-----------------------------------------------------------"
     curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | bash
+    echo "==========================================================="
+    echo "* Installing Extensions"
+    echo "-----------------------------------------------------------"
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-}
-
-setup-p10k() {
-    echo "==========================================================="
-    echo "* Installing Powerlevel10k..."
-    echo "-----------------------------------------------------------"
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-    cat $(pwd)/.p10k.zsh > $HOME/.p10k.zsh
 }
 
-zshrc() {
+copy-files() {
     echo "==========================================================="
-    echo "                  Import zshrc                   "
+    echo "                  Copying files                   "
     echo "-----------------------------------------------------------"
     cat $(pwd)/.zshrc > $HOME/.zshrc
+    cat $(pwd)/.p10k.zsh > $HOME/.p10k.zsh
+    cat $(pwd)/.vimrc > $HOME/.vimrc
+    cat $(pwd)/.gitconfig > $HOME/.gitconfig
 }
 
-cat $(pwd)/.vimrc > $HOME/.vimrc
 setup-omz
-setup-p10k
-zshrc
+copy-files
