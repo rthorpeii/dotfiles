454504761f78a3238b51c7267519a48e692140d0
Robert Thorpe II <rthorpeii@github.com>
2024-10-15 14:45:42 -0400

    Add alias for feature testing

--- zsh_custom/bash.zsh
+++ zsh_custom/bash.zsh
@@ -1,2 +1,3 @@
 alias rubocop-change='bin/rubocop -A $(git diff --name-only master..HEAD)'
+alias test-features='TEST_ALL_FEATURES=1 bin/rails test'
 alias update-owners='bin/generate-service-files.rb'
