27c2dc13cc7bf604ff253584f7f9e7e09d6e0af6
Robert Thorpe II <rthorpeii@github.com>
2025-05-08 11:18:59 -0400

    Add sorbet alias

--- zsh_custom/bash.zsh
+++ zsh_custom/bash.zsh
@@ -1,3 +1,4 @@
 alias rubocop-change='bin/rubocop -A $(git diff --name-only master..HEAD)'
 alias test-features='TEST_ALL_FEATURES=1 bin/rails test'
 alias update-owners='bin/generate-service-files.rb'
+alias update-sorbet='bin/rails db:migrate db:test:soft_reset; bin/tapioca dsl'
